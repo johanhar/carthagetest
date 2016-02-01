f172429ec64ee7d43a3d82909546dd23b8297678
Johannes Harestad <johannes.harestad@kantega.no>
2016-02-01 21:56:02 +0100

    Initial commit

---
+++ JHTabBar/JHTabBar.h
@@ -0,0 +1,19 @@
+//
+//  JHTabBar.h
+//  JHTabBar
+//
+//  Created by Johannes H on 01.02.2016.
+//  Copyright © 2016 Johannes Harestad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHTabBar.
+FOUNDATION_EXPORT double JHTabBarVersionNumber;
+
+//! Project version string for JHTabBar.
+FOUNDATION_EXPORT const unsigned char JHTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHTabBar/PublicHeader.h>
+
+
---
+++ JHTabBar/JHTabBar.swift
@@ -0,0 +1,19 @@
+//
+//  JHTabBar.swift
+//  JHTabBar
+//
+//  Created by Johannes H on 01.02.2016.
+//  Copyright © 2016 Johannes Harestad. All rights reserved.
+//
+
+import UIKit
+
+public class JHTabBar: UITabBarController {
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("✌️ Hey! 👍")
+    }
+
+}
